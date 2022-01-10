6883de5f77cf20b3ab205088e9b5d7904f7d5959
finney <finney@finneys-iMac.local>
2022-01-10 14:46:02 +0800

    data

---
+++ schnet/my_dataset/cal_hof.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8d1d09e63ad75181a594eacb7619fa75eea8a5e4e696eebb8e4eeb9177fc3673
+size 1303
---
+++ schnet/raw/gen_hof_data.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2cbf9f1a7078ed4f95bbd950f76a25a6e83aa78a3309807c920c4e0c4062d31a
+size 865
